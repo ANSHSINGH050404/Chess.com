053b7cfda9562ee8002e02796cfc4ef82ac0ab9d
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2026-05-26 20:06:13 +0530

    added frontend

---
+++ frontend/src/componets/ChessBoard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChessBoard = () => {
+  return (
+    <div className='bg-red-600'>ChessBoard</div>
+  )
+}
+
+export default ChessBoard
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import ChessBoard from "../componets/ChessBoard";
 const GamePage = () => {
   return (
-    <div>GamePage</div>
-  )
-}
+    <div className="flex justify-center pt-8 px-4">
+      <div className="flex flex-col lg:flex-row gap-8 max-w-screen-xl w-full">
+        <div className="flex-shrink-0">
+          <ChessBoard />
+        </div>
 
-export default GamePage
+        <div className="flex flex-col gap-4">
+          <button className="px-6 py-3 bg-[#86b32d] hover:bg-[#7ba32a] text-white font-bold rounded-lg transition cursor-pointer">
+            Play
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GamePage;
